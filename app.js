fe9548cd44ce9ade5c98dc5cff971b83c3e54bb0
calebdaisley1 <57918218+calebdaisley1@users.noreply.github.com>
2020-06-25 10:47:51 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,86 @@
+//Budget Controller
+var budgetController = (function() {
+    
+    // Some code
+    
+})();
+
+
+// UI Controller
+var UIController = (function() {
+    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    };
+    
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+                                                
+        },
+        
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+    
+
+})();
+
+
+//Global App Controller
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+        
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+        
+    
+        document.addEventListener('keypress', function(event) {
+        
+            if (event.keycode === 13 || event.which === 13) {
+                ctrlAddItem();
+            
+            }
+        
+        });
+    };
+    
+    
+    
+    var ctrlAddItem = function() {
+    
+        //1. Get input data
+        var input = UICtrl.getInput();
+
+        
+        //2. Add the item to the budget controller
+        
+        //3. Add item to the UI
+        
+        //4. Calculate the budget
+        
+        //5. Display the budget on the UI
+        
+ 
+    };
+    
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    };
+
+    
+})(budgetController, UIController);
+
+controller.init();
