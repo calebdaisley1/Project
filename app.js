0f659adc49a658c5d46f1c93b2e5c6b1884a5e5d
calebdaisley1 <57918218+calebdaisley1@users.noreply.github.com>
2020-06-25 14:14:09 -0400

    MVP
    Finished a minimum value project

--- app.js
+++ app.js
@@ -76,7 +76,12 @@ var budgetController = (function() {
             data.budget = data.totals.inc - data.totals.exp;
             
             // Calcualte the percentage of income spent
-            data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            if (data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+            
             
         },
         
@@ -107,7 +112,11 @@ var UIController = (function() {
         inputValue: '.add__value',
         inputButton: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
         
     };
     
@@ -160,6 +169,21 @@ var UIController = (function() {
             
         },
         
+        displayBudget: function(obj) {
+            
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+            
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+            
+        },
+        
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -198,7 +222,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
         
         //5. Display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
         
     };
     
@@ -230,6 +254,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     };
